8c6bbfe11272711e5c2e4021df5a86842b792029
Andrian Syah <74083728+Andriansyah2501@users.noreply.github.com>
2025-03-18 13:02:15 +0700

    Update apps.py

--- dashboard/apps.py
+++ dashboard/apps.py
@@ -8,7 +8,10 @@ st.title('🤖 Dashboard Visualisasi Data App')
 st.info('Hello Word')
 st.info('Dashboard Streamlit Andrian Syah')
 
-
+st.latex(r"""
+    \sum_{k=0}^{n-1} ar^k =
+    a \left(\frac{1-r^{n}}{1-r}\right)
+""")
 
 
 st.caption('Copyright (c) 2025')
