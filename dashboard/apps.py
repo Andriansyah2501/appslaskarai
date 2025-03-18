605705d1062930d4748b8a9d681aff30bbae52ca
Andrian Syah <74083728+Andriansyah2501@users.noreply.github.com>
2025-03-18 13:00:42 +0700

    Update apps.py

--- dashboard/apps.py
+++ dashboard/apps.py
@@ -8,6 +8,3 @@ st.title('🤖 Dashboard Visualisasi Data App')
 st.info('Hello Word')
 st.info('Dashboard Streamlit Andrian Syah')
 
-jumlah_kucing = np.array([3, 2, 1, 1, 2, 3, 2, 1, 0, 2])
-plt.hist(jumlah_kucing, bins=4)
-plt.show()
