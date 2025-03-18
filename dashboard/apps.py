f0de0258679694582c40ce090528247dd3348bba
Andrian Syah <74083728+Andriansyah2501@users.noreply.github.com>
2025-03-18 12:59:34 +0700

    Update apps.py

--- dashboard/apps.py
+++ dashboard/apps.py
@@ -2,13 +2,16 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 import streamlit as st
+import numpy as np
+import matplotlib.pyplot as plt
 
 st.title('🤖 Dashboard Visualisasi Data App')
 st.info('Hello Word')
 st.info('Dashboard Streamlit Andrian Syah')
 
+
+
+ 
 jumlah_kucing = np.array([3, 2, 1, 1, 2, 3, 2, 1, 0, 2])
 plt.hist(jumlah_kucing, bins=4)
 plt.show()
-
-
