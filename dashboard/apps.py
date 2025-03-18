698143c2868c6f259611211de167a1d990f61dcf
Andrian Syah <74083728+Andriansyah2501@users.noreply.github.com>
2025-03-18 13:04:47 +0700

    Update apps.py

--- dashboard/apps.py
+++ dashboard/apps.py
@@ -8,16 +8,6 @@ st.title('🤖 Dashboard Visualisasi Data App')
 st.info('Hello Word')
 st.info('Dashboard Streamlit Andrian Syah')
 
-with col1:
-    st.header("Kolom 1")
-    st.image("https://static.streamlit.io/examples/cat.jpg")
- 
-with col2:
-    st.header("Kolom 2")
-    st.image("https://static.streamlit.io/examples/dog.jpg")
- 
-with col3:
-    st.header("Kolom 3")
-    st.image("https://static.streamlit.io/examples/owl.jpg")
+
 
 st.caption('Copyright (c) 2025')
