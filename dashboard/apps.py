debbed13792f40be0787d01daffeef7194a03d1a
Andrian Syah <74083728+Andriansyah2501@users.noreply.github.com>
2025-03-18 12:44:08 +0700

    Update apps.py

--- dashboard/apps.py
+++ dashboard/apps.py
@@ -3,8 +3,8 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import streamlit as st
 
-st.title('🤖 Visualisasi Data App')
-st.info('Belajar Visualisasi Data')
+st.title('🤖 Dashboard Visualisasi Data App')
+st.info('Hello Word')
 st.info('Dashboard Streamlit Andrian Syah')
     
     
