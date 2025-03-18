21bcac8f37b2ae54fe0f7b3b0fb454fe20f017e5
Andrian Syah <74083728+Andriansyah2501@users.noreply.github.com>
2025-03-18 12:55:14 +0700

    Update apps.py

--- dashboard/apps.py
+++ dashboard/apps.py
@@ -7,11 +7,4 @@ st.title('🤖 Dashboard Visualisasi Data App')
 st.info('Hello Word')
 st.info('Dashboard Streamlit Andrian Syah')
 
-cities = ('Bogor', 'Bandung', 'Jakarta', 'Semarang', 'Yogyakarta', 
-          'Surakarta','Surabaya', 'Medan', 'Makassar')
- 
-populations = (45076704, 11626410, 212162757, 19109629, 50819826, 17579085,
-               3481, 287750, 785409)
- 
-plt.bar(x=cities, height=populations)
-plt.show()
+
